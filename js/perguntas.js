00d287b54754748a7693cbf5430919902a1eb4cc
Victor Hugo Lira De Souza <victor.lira@unifebe.edu.br>
2026-05-12 21:22:23 -0300

    curricularização v.1

---
+++ js/perguntas.js
@@ -0,0 +1,35 @@
+const perguntas = {
+    mundo1: [
+        {
+            pergunta: "BRUSQUE PERTENCE A QUAL ESTADO?",
+            opcoes: ["SÃO PAULO", "SANTA CATARINA", "BAHIA"],
+            resposta: 1
+        },
+        {
+            pergunta: "QUAL O MAIOR EVENTO CULTURAL DE BRUSQUE?",
+            opcoes: ["CARNAVAL", "FESTA JUNINA", "FENARRECO"],
+            resposta: 2
+        },
+        {
+            pergunta: "EXISTE UM LUGAR EM BRUSQUE ONDE VEMOS DIVERSOS ANIMAIS, COMO ARARAS, MACACOS E GRANDES FELINOS. QUAL O NOME DESSE LUGAR?",
+            opcoes: ["PARQUE DAS ESCULTURAS", "PRAIA", "PARQUE ZOO BOTÂNICO"],
+            resposta: 2
+        },
+        {
+            pergunta: "AO ANDAR PELA BEIRA-RIO, ÀS VEZES ENCONTRAMOS UM ANIMAL TÍPICO DA REGIÃO. QUAL É ESSE ANIMAL?",
+            opcoes: ["CAVALO", "CAPIVARA", "OVELHA"],
+            resposta: 1
+        },
+        {
+            pergunta: "QUAL O NOME DA PARTE DE BRUSQUE QUE FICA DIVIDIDA POR UM RIO?",
+            opcoes: ["BEIRA-RIO", "LIMEIRA", "SANTA RITA"],
+            resposta: 0
+        },
+    ],
+    mundo2: [
+        // adicione as perguntas do mundo 2 aqui
+    ],
+    mundo3: [
+        // adicione as perguntas do mundo 3 aqui
+    ]
+};
